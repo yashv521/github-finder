f64846f1902bca5a6aab91c8630b8e367dd9a9ad
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-11-25 19:56:59 +0530

    clear users and alert

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,10 +1,12 @@
 import { useState, useContext } from 'react'
 import GithubContext from '../../context/github/GithubContext'
+import AlertContext from '../../context/alert/AlertContext'
 
 function UserSearch() {
   const [text, setText] = useState('')
 
-  const { users } = useContext(GithubContext)
+  const { users, searchUsers, clearUsers } = useContext(GithubContext)
+  const {setAlert} = useContext(AlertContext)
 
   const handleChange = (e) => setText(e.target.value)
 
@@ -12,9 +14,9 @@ function UserSearch() {
     e.preventDefault()
 
     if (text === '') {
-      alert('Please enter something')
+      setAlert('Please enter something','error')
     } else {
-      // @todo - search users
+      searchUsers(text)
 
       setText('')
     }
@@ -42,7 +44,7 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button className='btn btn-ghost btn-lg'>Clear</button>
+          <button onClick={clearUsers} className='btn btn-ghost btn-lg'>Clear</button>
         </div>
       )}
     </div>
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,23 +14,33 @@ export const GithubProvider =({children}) => {
     
     const [state, dispatch] = useReducer(githubReducer,  initialState)
 
-    // Get initial users
-    const fetchUsers = async () => {
+    // Get search results
+    const searchUsers = async (text) => {
         setLoading()
 
-        const response = await fetch(`${GITHUB_URL}/users`, {
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             },
         })
 
-        const data = await response.json()
+        const {items} = await response.json()
 
        dispatch({
         type: 'GET_USERS',
-        payload: data,
+        payload: items,
        })
     }
+
+    //Clear users from state
+    const clearUsers = () => dispatch({type:
+        'CLEAR_USERS'
+    })
+
 
     //Set loading
     const setLoading = () => dispatch({type:
@@ -41,7 +51,9 @@ export const GithubProvider =({children}) => {
     value={{
         users: state.users,
         loading: state.loading,
-         fetchUsers}}>
+         searchUsers,
+         clearUsers,
+         }}>
         {children}
     </GithubContext.Provider>) 
 }
