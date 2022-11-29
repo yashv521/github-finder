edfbf4e1a42851dfbabe572a2fd3dbdc1e41d556
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-11-29 13:11:26 +0530

    git user repos

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,16 +1,18 @@
 import {FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
+import RepoList from '../components/repos/RepoList'
 import {useEffect, useContext} from 'react'
 import GithubContext from '../context/github/GithubContext'
 import {useParams} from 'react-router-dom'
 
 function User() {
-    const {getUser, user, loading} = useContext(GithubContext)
+    const {getUser, user, loading, getUserRepos, repos} = useContext(GithubContext)
 
     const params = useParams()
     useEffect(() => {
         getUser(params.login)
+        getUserRepos(params.login)
     }, [])
 
     const {
@@ -158,6 +160,8 @@ function User() {
                 </div>
             </div>
         </div>
+
+        <RepoList repos={repos}/>
     </div>
   </>
 
